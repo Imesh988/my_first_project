817a415eb859081fe69c69688c0ee24b5c232d32
imeshramanayaka@gmail.com <imeshramanayaka988@gmail.com>
2025-05-08 12:28:16 +0530

    third update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.getElementById('serviceForm').addEventListener('submit',function(e){
     e.preventDefault();
 
-    const cusNIC = document.getElementById('cus_nic');
-    const vehNo = document.getElementById('veh_no');
-    const vehName = document.getElementById('vehi_name');
-    const date = document.getElementById('date');
-    const cost = document.getElementById('cost');
+    const cusNIC = document.getElementById('cus_nic').value;
+    const vehNo = document.getElementById('veh_no').value;
+    const vehName = document.getElementById('vehi_name').value;
+    const date = document.getElementById('date').value;
+    const cost = document.getElementById('cost').value;
 
     const cusDetails = {cusNIC,vehNo,vehName,date,cost};
 
     localStorage.setItem(cusNIC, JSON.stringify(cusDetails));
-    alert("customer details successfull !!");
+    alert("Customer Details Successfull !!");
     document.getElementById('serviceForm').reset();
 });
 
@@ -24,6 +24,42 @@ function searchCustomer(){
     if(customerData){
         const cusDetails = JSON.parse(customerData);
 
-        
+        document.getElementById('result').innerHTML = `
+        <p><strong>Customer NIC : </strong>${cusDetails.cusNIC}</p>
+        <p><strong>Vehical Number : </strong>${cusDetails.vehNo}</p>
+        <p><strong>Vehical Name : </strong>${cusDetails.vehName}</p>
+        <p><strong>Service Cost : </strong>${cusDetails.cost}</p>`;
+
+        document.getElementById('costCalculate').style.display = 'block';
+    }else{
+
+        document.getElementById('result').innerHTML = `
+        <p class="text-danger">Customer Details Not Found !!</p>
+        `;
+
+        document.getElementById('costCalculate').style.display = 'none';
     }
+
+   
+
+}
+
+//full cost calculate
+function fullCost(){
+    const searchId = document.getElementById('serchId').value;
+    const customerData = localStorage.setItem(searchId);
+
+    if(customerData){
+        const customer  = JSON.parse(customerData);
+
+        const extraCost = parseFloat(document.getElementById('full_cost').value);
+
+        const fullCost = parseFloat(cusDetails.cost)* extraCost;
+
+        document.getElementById('full_cost').innerHTML = `
+        <hr>
+        <h4><strong>Full Cost : Rs.${fullCost}</strong></h4>
+        `;
+    }
+
 }
